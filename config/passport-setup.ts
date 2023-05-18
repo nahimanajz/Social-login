613f3d053034db634f1a56e1bb19cc11821b6207
nahimanajz <jazzonahimana@gmail.com>
2023-05-18 17:53:36 +0200

    enable ts

---
+++ config/passport-setup.ts
@@ -0,0 +1,30 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+try {
+    
+    passport.use(
+        new GoogleStrategy({
+        //options for the strategy
+        callbackURL:'http://localhost:7878/auth/google/redirect',
+        clientID: process.env.GOOGLE_SIGNIN_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_SIGNIN_CLIENT_SECRET
+    },(acessToken, refreshToken, profile, done)=>{
+        // Identified issue redirection is not working
+        // passport callback function
+        //TODO: CHECK if user exist then save user into db happens here 
+        //console.log("Passport callback function fired")
+       // NB: SIGNIN AND SIGNUP ARE HANDLED HERE NOTHING MATCH
+        console.log(profile)
+
+        done(null, profile)
+        
+    })
+    
+    )
+} catch (error) {
+    console.log(error)
+}
